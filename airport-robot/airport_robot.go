c8d40535d4a43c3f41ea6bd1e6c5c2d562329ccf
Manethpak <manethpak00@gmail.com>
2023-02-11 01:32:30 +0700

    submit interface

--- airport-robot/airport_robot.go
+++ airport-robot/airport_robot.go
@@ -1,5 +1,40 @@
 package airportrobot
 
+import (
+	"fmt"
+)
+
 // Write your code here.
 // This exercise does not have tests for each individual task.
 // Try to solve all the tasks first before running the tests.
+
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+func SayHello(name string, greeter Greeter) string {
+	return greeter.Greet(name)
+}
+
+type Italian struct {
+}
+
+func (i Italian) Greet(name string) string {
+	return fmt.Sprintf("I can speak Italian: Ciao %s!", name)
+}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+
+type Portuguese struct {
+}
+
+func (p Portuguese) Greet(name string) string {
+	return fmt.Sprintf("I can speak Portuguese: Olá %s!", name)
+}
+
+func (p Portuguese) LanguageName() string {
+	return "Portuguese"
+}
